a364196814c3b33e7fd51a42b4c3a48a3aaeaee8
RealDolos <dolosthegreat@safe-mail.net>
2016-08-28 18:35:44 +0200

    Update list of extraordinary gentlemen

--- volaparrot/constants.py
+++ volaparrot/constants.py
@@ -2,9 +2,9 @@ ADMINFAG = ["RealDolos"]
 PARROTFAG = "Parrot"
 
 BLACKFAGS = [i.casefold() for i in (
-    "kalyx", "merc", "loliq", "annoying", "bot", "RootBeats", "JEW2FORU", "quag", "mire", "perici")]
+    "kalyx", "merc", "loliq", "annoying", "RootBeats", "JEW2FORU", "quag", "mire", "perici", "Voldemort", "briseis", "brisis", "GNUsuks", "rhooes", "n1sm4n", "honeyhole", "Printer", "yume1")]
 OBAMAS = [i.casefold() for i in (
-    "counselor", "briseis", "apha", "bread", "ark3", "jizzbomb", "acid", "elkoalemos", "tarta")]
+    "counselor", "briseis", "apha", "bread", "ark3", "jizzbomb", "acid", "elkoalemos", "tarta", "counselor", "myon")]
 
-BLACKROOMS = "e7u-CG", "jAzmc3", "f66jeG", "24_zFd"
-WHITEROOMS = "9pdLvy"
+BLACKROOMS = "e7u-CG", "jAzmc3", "f66jeG", "24_zFd", "BHfjGvT", "BHI0pxg",
+WHITEROOMS = "BEEPi",
