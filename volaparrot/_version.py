a55238c77b5fe451e83ee21eba3e2252975a2cf8
RealDolos <dolosthegreat@safe-mail.net>
2017-01-02 17:33:14 +0100

    Show versions

--- volaparrot/_version.py
+++ volaparrot/_version.py
@@ -1,5 +1,7 @@
+import volapi.volapi as va
+
 __all__ = ["__version__", "__title__", "__fulltitle__"]
 
 __version__ = "1.0"
 __title__ = "The One Good Parrot"
-__fulltitle__ = "{} - {} {}".format(__package__, __title__, __version__)
+__fulltitle__ = "{} - {} {} on volapi {}".format(__package__, __title__, __version__, va.__version__)
