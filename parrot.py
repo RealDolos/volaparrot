10bfcdce235ea451ddd765011551af419d78383c
RealDolos <dolosthegreat@safe-mail.net>
2017-01-02 17:57:06 +0100

    Bump versions
    and remove parrot.py

--- parrot.py
+++
@@ -1,4 +0,0 @@
-from volaparrot.__main__ import run
-
-if __name__ == "__main__":
-    run()
--- volaparrot/_version.py
+++ volaparrot/_version.py
@@ -2,6 +2,6 @@ import volapi.volapi as va
 
 __all__ = ["__version__", "__title__", "__fulltitle__"]
 
-__version__ = "1.0"
+__version__ = "2.0"
 __title__ = "The One Good Parrot"
 __fulltitle__ = "{} - {} {} on volapi {}".format(__package__, __title__, __version__, va.__version__)
