8af181b9419a87489ad2f32c5780668b29edb285
RealDolos <dolos@cock.li>
2017-04-27 03:40:21 +0200

    3.0

--- volaparrot/_version.py
+++ volaparrot/_version.py
@@ -2,7 +2,7 @@ import volapi.volapi as va
 
 __all__ = ["__version__", "__title__", "__fulltitle__"]
 
-__version__ = "2.0"
+__version__ = "3.0"
 __title__ = "The One Good Parrot"
 __fulltitle__ = "{} - {} {} on volapi {}".format(
     __package__, __title__, __version__, va.__version__)
